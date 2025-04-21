b3871cce8611060935262409faabb89542b5bcac
jhJohnson16 <jacob.johnson4@stu.daviess.kyschools.us>
2025-04-21 04:00:48 +0000

    Coding with Crazy Cattle 3D withdrawals

--- AInsertionSort.java
+++ AInsertionSort.java
@@ -5,5 +5,19 @@ public class AInsertionSort
     {
     	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
 
+        int tempNum = 0;
+
+        for(int i=1; i<myNumbers.length; i++){
+            int spot = 1;
+
+            while(myNumbers[spot] < myNumbers[spot-1] && spot >= 0){
+                tempNum = myNumbers[spot-1];
+                myNumbers[spot-1] = myNumbers[spot];
+                myNumbers[spot] = tempNum;
+
+                spot --;
+            }
+        }
+        System.out.println("Sorted Array: " + Arrays.toString(myNUmbers));
     }
 }
--- ALInsertionSort.java
+++ ALInsertionSort.java
@@ -8,18 +8,35 @@ public class ALInsertionSort
     	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
     	ArrayList<Integer> sorted = new ArrayList<Integer>();
     	boolean inserted=false;
+		System.out.println("Unsorted Aray " + Arrays.toString(myNumners));
 
 		//Add the first number to the array list
-
-		//Loop once for all of the remaining numbers in the unsorted list
+			sorted.add(myNumbers(0));
 
 			//Check with each of the numbers in the sorted list
+			for(int i=1; i<myNumbers.length; i++){
+				inserted = false;
+				int spot = 0;
 
+				while(inserted = false && spot < sorted.size()){
+					if(myNumbers[i] < sorted.get(spot)){
+						sorted.add(spot, myNumbers[i]);
+						inserted = true;
+					}
+					spot++;
+				}
+				if(inserted == false)
+					sorted.add(myNumbers[i]);
+			}
+				for(int j = 0; j<myNumbers.length; j++){
+					myNumbers[j] = sorted.get(j);
+				}
 				//If this number is less than one in the sorted list,
+				
 				//insert it there
 
 			//If it was not inserted, stick it on the end.
-
+				
 
 		//End Loop for unsorted list
 
@@ -28,7 +45,7 @@ public class ALInsertionSort
 		//Move the data back to the array
 
 		//Print the contents of the array
-
+		System.out.println("Sprted Array: " + Array.toString(myNumbers));
     }
 
 
